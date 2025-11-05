328e57f184205e254bdbfb573ad592869b901bfb
unnecessary11223-rgb <unnecessary11223@gmail.com>
2025-11-05 21:54:29 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE='vault-v4'; // bump when you change anything
-const CORE=['./','./index.html','./app.js','./db.js','./auth.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{
-    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request, copy)); return res;
+const CACHE = 'vault-v7'; // bump this if you change files
+const CORE = ['.','./index.html','./app.js','./db.js','./auth.js','./runner.js','./manifest.webmanifest'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE))); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))); });
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+    const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return res;
   })));
 });
